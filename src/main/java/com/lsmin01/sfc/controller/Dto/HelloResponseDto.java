0f5806ce09df08061fbc57b4532ec30452800825
LeeSungmin <lsmin01@gmail.com>
2020-01-17 01:04:54 +0900

    add lombok

---
+++ src/main/java/com/lsmin01/sfc/controller/Dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.lsmin01.sfc.controller.Dto;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloResponseDto {
+
+    private String name;
+    private int amount;
+
+}
---
+++ src/main/java/com/lsmin01/sfc/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.lsmin01.sfc.controller;
+
+import com.lsmin01.sfc.controller.Dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto HelloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+
+}
